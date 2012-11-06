ce9d14389f0cb06315b2b11481a8209d50d2fe07
unknown <Administrator@CAT.(none)>
2012-11-06 22:59:51 +0800

    send hex data added

---
+++ bin/classes.dex.d
@@ -0,0 +1,22 @@
+C:\Documents and Settings\Administrator\My Documents\NetBeansProjects\bluetoothchat\bin\classes.dex : \
+C:\Documents and Settings\Administrator\My Documents\NetBeansProjects\bluetoothchat\bin\classes\com\example\android\BluetoothChat\BluetoothChat$1.class \
+C:\Documents and Settings\Administrator\My Documents\NetBeansProjects\bluetoothchat\bin\classes\com\example\android\BluetoothChat\BluetoothChat$2.class \
+C:\Documents and Settings\Administrator\My Documents\NetBeansProjects\bluetoothchat\bin\classes\com\example\android\BluetoothChat\BluetoothChat$3.class \
+C:\Documents and Settings\Administrator\My Documents\NetBeansProjects\bluetoothchat\bin\classes\com\example\android\BluetoothChat\BluetoothChat.class \
+C:\Documents and Settings\Administrator\My Documents\NetBeansProjects\bluetoothchat\bin\classes\com\example\android\BluetoothChat\BluetoothChatService$AcceptThread.class \
+C:\Documents and Settings\Administrator\My Documents\NetBeansProjects\bluetoothchat\bin\classes\com\example\android\BluetoothChat\BluetoothChatService$ConnectedThread.class \
+C:\Documents and Settings\Administrator\My Documents\NetBeansProjects\bluetoothchat\bin\classes\com\example\android\BluetoothChat\BluetoothChatService$ConnectThread.class \
+C:\Documents and Settings\Administrator\My Documents\NetBeansProjects\bluetoothchat\bin\classes\com\example\android\BluetoothChat\BluetoothChatService.class \
+C:\Documents and Settings\Administrator\My Documents\NetBeansProjects\bluetoothchat\bin\classes\com\example\android\BluetoothChat\BuildConfig.class \
+C:\Documents and Settings\Administrator\My Documents\NetBeansProjects\bluetoothchat\bin\classes\com\example\android\BluetoothChat\DeviceListActivity$1.class \
+C:\Documents and Settings\Administrator\My Documents\NetBeansProjects\bluetoothchat\bin\classes\com\example\android\BluetoothChat\DeviceListActivity$2.class \
+C:\Documents and Settings\Administrator\My Documents\NetBeansProjects\bluetoothchat\bin\classes\com\example\android\BluetoothChat\DeviceListActivity$3.class \
+C:\Documents and Settings\Administrator\My Documents\NetBeansProjects\bluetoothchat\bin\classes\com\example\android\BluetoothChat\DeviceListActivity.class \
+C:\Documents and Settings\Administrator\My Documents\NetBeansProjects\bluetoothchat\bin\classes\com\example\android\BluetoothChat\R$attr.class \
+C:\Documents and Settings\Administrator\My Documents\NetBeansProjects\bluetoothchat\bin\classes\com\example\android\BluetoothChat\R$drawable.class \
+C:\Documents and Settings\Administrator\My Documents\NetBeansProjects\bluetoothchat\bin\classes\com\example\android\BluetoothChat\R$id.class \
+C:\Documents and Settings\Administrator\My Documents\NetBeansProjects\bluetoothchat\bin\classes\com\example\android\BluetoothChat\R$layout.class \
+C:\Documents and Settings\Administrator\My Documents\NetBeansProjects\bluetoothchat\bin\classes\com\example\android\BluetoothChat\R$menu.class \
+C:\Documents and Settings\Administrator\My Documents\NetBeansProjects\bluetoothchat\bin\classes\com\example\android\BluetoothChat\R$string.class \
+C:\Documents and Settings\Administrator\My Documents\NetBeansProjects\bluetoothchat\bin\classes\com\example\android\BluetoothChat\R.class \
+C:\Program Files\Android\android-sdk\tools\support\annotations.jar \
---
+++ bin/controller-debug-unaligned.apk.d
@@ -0,0 +1,4 @@
+C:\Documents and Settings\Administrator\My Documents\NetBeansProjects\bluetoothchat\bin\controller-debug-unaligned.apk : \
+C:\Documents and Settings\Administrator\My Documents\NetBeansProjects\bluetoothchat\bin\controller.ap_ \
+C:\Documents and Settings\Administrator\My Documents\NetBeansProjects\bluetoothchat\bin\classes.dex \
+C:\Program Files\Android\android-sdk\tools\support\annotations.jar \
